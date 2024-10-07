3b8e59c0b5213f935c6e9a4fe5072e2abcbe716d
Zátrok Tamás <blutengel.144@gmail.com>
2024-10-07 15:09:58 +0200

    chapter 10

--- src/pages/portfolio/Software.jsx
+++ src/pages/portfolio/Software.jsx
@@ -1,8 +1,6 @@
 import software1 from './software/1.jpg'
 import software2 from './software/2.jpg'
 import software3 from './software/3.jpg'
-import software4 from './software/4.jpg'
-import software5 from './software/5.jpg'
 import { Link } from "react-router-dom";
 
 function Software() {
@@ -28,14 +26,6 @@ function Software() {
 
                     <div className="col">
                         <a target="_blank" href={"https://zatroktamas01.github.io/pokemonlist/"}><img src={software3} className="gallery-item bg-dark" alt="gallery" /></a>
-                    </div>
-
-                    <div className="col">
-                        <a target="_blank" href={"https://basic-calculator-02.netlify.app/"}><img src={software4} className="gallery-item bg-dark" alt="gallery" /></a>
-                    </div>
-
-                    <div className="col">
-                        <a target="_blank" href={"https://zatroktamas01.github.io/SimonGame/"}><img src={software5} className="gallery-item bg-dark" alt="gallery" /></a>
                     </div>
 
                     <div className="button-container">
